f550aaf85cd7d613835efe678adbd64c5ae48b5b
AceeeDev <cyberandrew03@gmail.com>
2025-03-03 00:09:45 -0800

    Created super basic pages

--- bronconest_app/lib/app.dart
+++ bronconest_app/lib/app.dart
@@ -7,7 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'BroncoNest',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
---
+++ bronconest_app/lib/pages/welcome_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({super.key});
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Welcome Page'),
+      ),
+      body: Center(child: const Text('Text!')),
+    );
+  }
+}
